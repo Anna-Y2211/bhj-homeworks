91a3e3de89769930b1f7b16f622ab79e145d6a43
AnnaYou <anna2211985@ya.ru>
2024-12-07 19:59:22 +0300

    Доработанное второе задание

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,9 +3,10 @@ clicker.onclick = function() {
     const counter = document.getElementById('clicker__counter');
     counter.textContent++;
 
-    if(counter.textContent == true) {
-        clicker.width = 250;
-    } else {
-        clicker.width = 200;
-    }
+
+if(counter.textContent % 2) {
+    clicker.width = 250;
+ }else {
+    clicker.width = 200;
+ }
 }
