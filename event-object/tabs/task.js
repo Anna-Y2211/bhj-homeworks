5fa264bd393e837838154a7b8c79c1dcb5a4eb89
AnnaYou <anna2211985@ya.ru>
2025-01-16 19:26:16 +0300

    new

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,14 +1,15 @@
 const tabs = Array.from(document.querySelectorAll('.tab'));
 const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
 const tabActive = document.querySelector('.tab_active');
+const activeContent = document.querySelector('.tab__content_active');
 
-const findTab = tabs.findIndex((tabActive) => {
-	tabActive.addEventListener('click', () => {
-		if(tabActive) {
-			tabActive.classList.remove('tab_active');
-			tabActive.classList.add('tab_active')
-		}
-})
+const findTabs = tabs.forEach((el) => {
+	el.addEventListener('click', () => {
+	const active = tabs.findIndex((tabActive) => {
+	tabActive.classList.remove('tab_active');
+	active.classList.remove('tab_active')
 
+	})
+	el.classList.add('tab_active')
 })
-	
+})
