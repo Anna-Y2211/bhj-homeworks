c430ef936f56c4f9922c979267f1f5ff8749b303
AnnaYou <anna2211985@ya.ru>
2025-02-10 20:07:55 +0300

    new file

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,22 +1,20 @@
 const tabs = Array.from(document.querySelectorAll('.tab'));
-const tabsContent = Array.from(document.querySelectorAll('.tab__content'));
-let tabActive = document.querySelector('.tab_active');
-let activeContent = document.querySelector('.tab__content_active');
+const tabsContent = document.querySelectorAll('.tab__content');
+const tabActive = document.querySelector('.tab_active');
+const activeContent = document.querySelector('.tab__content_active');
+
 
 tabs.forEach((tab) => {
-	tab.addEventListener('click', () => {
-	  let indexTab = tabs.findIndexf((el) => {
-		el.classList.remove('tab_active')
-		indexTab.classList.remove('tab_active')
-		tab.classList.add('tab_active')
-		indexTab.classList.add('tab_active')
-	  });
-	  //
-	 // tabActive.classList.remove[indexTab];
-	  //tabActive.classList.add('tab_active');
+  tab.addEventListener('click', (e) => {
+  let indexTab = tabs.findIndex((tabActive) => {
+  tabActive.classList.remove('tab_active');
+  tab.classList.add('tab_active') 
+   
+    tabActive.classList.remove('tab__content_active')
+    tab.classList.add('tab__content_active')
+    
+    
+})
   
-	  //activeContent.classList.remove('tab__content_active');
-	  //activeContent = tabsContent[indexTab];
-	  //activeContent.classList.add('tab__content_active');
-	});
-  });
+})
+})  
