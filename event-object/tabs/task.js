71630016fba24846578cd5385d7d1655eb442e76
AnnaYou <anna2211985@ya.ru>
2025-02-12 23:24:03 +0300

    Новый коммит

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,15 +1,16 @@
 const tabs = Array.from(document.querySelectorAll('.tab'));
 const tabsContent = document.querySelectorAll('.tab__content');
-const tabActive = document.querySelector('.tab_active');
+//const tabActive = document.querySelector('.tab_active');
 const activeContent = document.querySelector('.tab__content_active');
 
 
 tabs.forEach((tab) => {
   tab.addEventListener('click', (e) => {
-   const indexTab = tabs.findIndex((element) => element == tabActive)
-       tab.classList.remove('tab_active')
-       indexTab.classList.add('tab_active')
-    
+
+const indexTab = tabs.findIndex((element) => {
+if(element.classList.contains('tab_active')){
+  return element
+}
 })
-  
+})
 })
