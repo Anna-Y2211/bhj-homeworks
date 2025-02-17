f59e5270cea3dde5b84195160a92b74099ce5ba1
AnnaYou <anna2211985@ya.ru>
2025-02-17 20:12:03 +0300

    new commit

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,16 +1,24 @@
 const tabs = Array.from(document.querySelectorAll('.tab'));
 const tabsContent = document.querySelectorAll('.tab__content');
 //const tabActive = document.querySelector('.tab_active');
-const activeContent = document.querySelector('.tab__content_active');
+//const activeContent = document.querySelector('.tab__content_active');
+
 
 
 tabs.forEach((tab) => {
-  tab.addEventListener('click', (e) => {
-
+   tab.addEventListener('click', () => {
+  
 const indexTab = tabs.findIndex((element) => {
-if(element.classList.contains('tab_active')){
-  return element
-}
+  if(element.classList.contains('tab_active')) {
+    return true
+  }else{
+    return false
+  }
 })
-})
-})
+indexTab.classList.remove('tab_active')
+tabsContent.classList.remove('tab__content_active')  
+tab.classList.add('tab_active')
+tab.classList.add('tab__content_active')
+   })
+  })
+  
